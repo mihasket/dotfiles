cefa6fe70e496d8bda71ba312018c7c4503013a7
mihasket <miha.sket@gmail.com>
2022-07-03 15:19:50 +0200

    Add polybar config

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+polybar-msg cmd quit
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch
+polybar main &
+
+echo "Bar launched..."
